f226b03e8432b7182d44cdee0367386708fb2e08
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-05-29 07:48:19 +0530

    Added ModelForm Example and response.save()

--- my_site/carrental/forms.py
+++ my_site/carrental/forms.py
@@ -1,9 +1,38 @@
 from django import forms
+from .models import Feedback
+from django.forms import ModelForm
 
+STARS = [
+    (0, "0"),
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+]
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label="First name", max_length=100)
-    last_name = forms.CharField(label="Last name", max_length=100)
-    email_name = forms.EmailField(label="Email", max_length=100)
-    review = forms.CharField(label="Kindly review", max_length=1000)
-    stars = forms.IntegerField(label="Stars", min_value=0, max_value=5)
+# DJANGO FORMS EXAMPLE BELOW
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label="First name", max_length=100)
+#     last_name = forms.CharField(label="Last name", max_length=100)
+#     email_name = forms.EmailField(label="Email", max_length=100)
+#     stars = forms.ChoiceField(choices=STARS)
+#     review = forms.CharField(label="Kindly review", max_length=1000,
+#             widget=forms.Textarea(attrs={"class": "myform",
+#                                          "rows": 5,
+#                                          "columns": 2}))
+    
+# DJANGO MODEL FORM
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Feedback
+        labels = {
+            "first_name": "Kya bolti public"
+        }
+        fields = ["first_name", "last_name", "email", "stars", "review"] #  or "__all__" for inclusing everything
+        # https://docs.djangoproject.com/en/4.2/ref/forms/fields/#charfield
+        error_messages = {
+            "first_name": {
+                "required": "Naam bol apna, bhidu!"
+            }
+        }
--- my_site/carrental/models.py
+++ my_site/carrental/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+STARS = [
+    ("0", "0"),
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+]
 # Create your models here.
 class Feedback(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    stars = models.CharField(choices=STARS, max_length=1)
     review = models.TextField(max_length=1000)
-    stars = models.IntegerField()
 
     def __str__(self) -> str:
         return f'Its a {self.stars} star review !!'
